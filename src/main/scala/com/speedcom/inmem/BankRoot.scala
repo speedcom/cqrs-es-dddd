57adb8fd8d4a85dd4fd1b9a607cc52cf2b9b96d4
speedcom <mateusz.maciaszekhpc@gmail.com>
2015-04-06 18:27:55 +0200

    new bank root structure

--- src/main/scala/com/speedcom/inmem/BankRoot.scala
+++ src/main/scala/com/speedcom/inmem/BankRoot.scala
@@ -7,21 +7,21 @@ import com.speedcom.core.transaction_history.boundary.TransactionHistory
 // OUTER WORLD - MUTABLE ONE
 class BankRoot {
 
-  var profiles = Map[ProfileId, Profile](
-    ProfileId(1L) -> Profile(ProfileId(1L), "Mateusz", "Maciaszek"),
-    ProfileId(2L) -> Profile(ProfileId(2L), "Justyna", "Grudzien")
+  var profiles = Seq(
+    Profile(ProfileId(1L), "Mateusz", "Maciaszek"),
+    Profile(ProfileId(2L), "Justyna", "Grudzien")
   )
 
-  var bankAccounts = Map[BankAccountNumber, BankAccount](
-    BankAccountNumber("5555"*5) -> BankAccount(ProfileId(1L), BankAccountNumber("5555"*5), "millionaire"),
-    BankAccountNumber("3454"*5) -> BankAccount(ProfileId(1L), BankAccountNumber("3454"*5), "normal"),
-    BankAccountNumber("1111"*5) -> BankAccount(ProfileId(2L), BankAccountNumber("3454"*5), "millionaire")
+  var bankAccounts = Seq(
+    BankAccount(ProfileId(1L), BankAccountNumber("5555"*5), "millionaire"),
+    BankAccount(ProfileId(1L), BankAccountNumber("3454"*5), "normal"),
+    BankAccount(ProfileId(2L), BankAccountNumber("3454"*5), "millionaire")
   )
 
-  var transactionHistories = Map[BankAccountNumber, TransactionHistory](
-    BankAccountNumber("5555"*5) -> TransactionHistory(BankAccountNumber("5555"*5), transactions = List.empty),
-    BankAccountNumber("3454"*5) -> TransactionHistory(BankAccountNumber("3454"*5), transactions = List.empty),
-    BankAccountNumber("1111"*5) -> TransactionHistory(BankAccountNumber("1111"*5), transactions = List.empty)
+  var transactionHistories = Seq(
+    TransactionHistory(BankAccountNumber("5555"*5), transactions = List.empty),
+    TransactionHistory(BankAccountNumber("3454"*5), transactions = List.empty),
+    TransactionHistory(BankAccountNumber("1111"*5), transactions = List.empty)
   )
 
 }
